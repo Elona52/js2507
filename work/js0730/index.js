69618f2d13138e2da4c8b71781a0651c31e82570
ELORA <dlwlals52@gmail.com>
2025-07-30 16:16:05 +0900

    과제체출0730

---
+++ work/js0730/index.js
@@ -0,0 +1,22 @@
+let text = ""
+let mytext = ""
+
+// consol.log 에 cars 를 쓰면 밑에 변수값들이 자동으로 불러와진다.
+// cars = [0 , 1 , 2 , 3 , 4 , 5]
+const cars = ["BMW","볼보","사브","포드","피아트","아우디"]
+// 상수 :  저장공간을 픽스해두기 위해서 사용하는 값
+// for(초기값;실행;증감값)
+for (let i = 0; i < cars.length; i++){
+   // console.log(i);
+   // console.log(cars[i])   
+    mytext += cars[i] + "_"
+    console.log(cars[i])
+}
+
+// ""+"BMW" = "BMW" : text += cars[0]
+// "BMW" + "볼보"= "BMW볼보" : text += cars[1] + "_"
+// text += cars[2] + "_"
+// text += cars[3] + "_"
+// text += cars[4] + "_"
+// text += cars[5] + "_"
+
---
+++ work/js0730/index2.js
@@ -0,0 +1,28 @@
+// if(조건문)
+let myDate = new Date()
+let myhour = myDate.getHours()
+// class를 만들 수 있는 객체, object를 만들 수 있는 class - class를 만들때 대문자를 씀.
+let greeting = ""
+// 가데이터 : 가상의 가짜값으로 테스트를 해보세요
+let timeAM = 9
+let timePM = 18
+// 가데이터로 테스트
+myhour = 7
+
+console.log(myDate)
+console.log(myhour)
+
+if(myhour < timeAM){
+    greeting = "등원"
+//else if(또다른 조건문)
+}else if(myhour > timePM)
+    greeting = "굿귀가"
+//else(조건에 해당되지 않으면 )
+else{
+    greeting = "열공\n화이팅"
+}
+console.log(greeting)
+
+
+
+
