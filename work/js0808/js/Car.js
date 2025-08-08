e3f5c74d3dab3f00d6e5e4b3cee256808abe133d
ELORA <dlwlals52@gmail.com>
2025-08-08 16:37:06 +0900

    0808

---
+++ work/js0808/js/Car.js
@@ -0,0 +1,10 @@
+//차 컴포넌트 구성부
+function Car(pr) {
+    return (
+        <>
+            <h2>There is {pr.color} {pr.name} car in garage</h2>
+            <p>Nice to meet you</p>
+        </>
+    )
+}
+export default Car
